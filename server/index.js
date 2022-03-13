ff6d8b27cf3677c3c02f6720c5a3f12ebfa4b44c
miltonbolonha <miltonbolonha@gmail.com>
2022-03-13 12:27:57 -0300

    Server Static Functions

--- server/index.js
+++ server/index.js
@@ -4,12 +4,11 @@ const gatsby = require('gatsby-plugin-nodejs')
 const app = express()
 
 gatsby.prepare({ app }, () => {
-	// Here you can define your routes
+	app.get('/darkness', function (req, res) {
+		res.send('hello world')
+	})
 })
 
 const port = process.env.PORT || 1337
 
 app.listen(port, () => console.log(`listening on port ${port}`))
-app.get('/darkness', function (req, res) {
-	res.send('hello world')
-})
